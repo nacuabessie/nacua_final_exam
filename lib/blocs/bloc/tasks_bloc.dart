ed648f9d8fdc69e5ed344ef35851686343902e1d
bessie nacua <bessie.nacua@usjr.edu.ph>
2022-08-04 13:02:30 +0800

    Can delete Task

--- lib/blocs/bloc/tasks_bloc.dart
+++ lib/blocs/bloc/tasks_bloc.dart
@@ -1,12 +1,13 @@
-import 'package:bloc/bloc.dart';
+
 import 'package:equatable/equatable.dart';
 
 import '../../models/task.dart';
+import '../bloc_exports.dart';
 
 part 'tasks_event.dart';
 part 'tasks_state.dart';
 
-class TasksBloc extends Bloc<TasksEvent, TasksState> {
+class TasksBloc extends HydratedBloc<TasksEvent, TasksState> {
   TasksBloc() : super(const TasksState()) {
     on<AddTask>(_onAddTask);
     on<UpdateTask>(_onUpdateTask);
@@ -31,5 +32,19 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
   
   emit(TasksState(allTasks: allTasks));
   }
-  void _onDeleteTask(DeleteTask event, Emitter<TasksState> emit){}
+  void _onDeleteTask(DeleteTask event, Emitter<TasksState> emit){
+        final state = this.state;
+    emit(TasksState(allTasks: List.from(state.allTasks)..remove(event.task),
+    ));
+  }
+  
+  @override
+  TasksState? fromJson(Map<String, dynamic> json) {
+    return TasksState.fromMap(json);
+  }
+  
+  @override
+  Map<String, dynamic>? toJson(TasksState state) {
+    return state.toMap();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'app_router.dart';
 import 'app_themes.dart';
-import 'blocs/bloc/tasks_bloc.dart';
-import 'models/task.dart';
+import 'blocs/bloc_exports.dart';
 import 'screens/tabs_screen.dart';
 
-void main() {
-  BlocOverrides.runZoned(() => runApp(
-      MyApp(appRouter: AppRouter()),
-  ));
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory());
+
+  HydratedBlocOverrides.runZoned(
+    () => runApp(MyApp(appRouter: AppRouter())),
+        storage: storage,
+      );
 }
 
 class MyApp extends StatelessWidget {
@@ -20,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => TasksBloc()..add(AddTask(task: Task(title: 'Task1', description: ''))),
+      create: (context) => TasksBloc(),
       child: MaterialApp(
         title: 'BloC Tasks App',
         theme: AppThemes.appThemeData[AppTheme.lightMode],
